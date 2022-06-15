ca699c22ea0ba12996240731acb337172d128db2
Mizael Nunes <mizaelnunes001@hotmail.comgit>
2022-06-15 14:16:44 -0300

    cadastro de part.evento

---
+++ atividade2.js
@@ -0,0 +1,9 @@
+// Cadastro de Participantes do Evento.
+ let nome = "Ana ";
+ console.log(nome);
+ let idade = 31;
+if(idade>= 18){
+console.log(nome + 'Cadastro Realizado com sucesso, Bem Vindo ao Evento ');    
+} else {
+    console.log(nome + 'menor de idade não pode cadastrar')
+}
